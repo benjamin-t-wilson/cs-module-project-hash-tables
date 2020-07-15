df9a72de9eb1d7649a29bb1c45306cf985a2f6f9
benjamin-t-wilson <benjiimanwillson@gmail.com>
2020-07-14 17:28:25 -0700

    finished lookup table

--- applications/lookup_table/lookup_table.py
+++ applications/lookup_table/lookup_table.py
@@ -1,5 +1,6 @@
 # Your code here
-
+import random
+import math
 
 def slowfun_too_slow(x, y):
     v = math.pow(x, y)
@@ -9,12 +10,23 @@ def slowfun_too_slow(x, y):
 
     return v
 
+cache = {}
+
 def slowfun(x, y):
     """
     Rewrite slowfun_too_slow() in here so that the program produces the same
     output, but completes quickly instead of taking ages to run.
     """
     # Your code here
+    if f'{x},{y},pow' not in cache:
+        cache[f'{x},{y},pow'] = math.pow(x, y)
+    if f'{x},{y},fac' not in cache:
+        cache[f'{x},{y},fac'] = math.factorial(cache[f'{x},{y},pow'])
+    if f'{x},{y},div' not in cache:
+        cache[f'{x},{y},div'] = cache[f'{x},{y},fac'] // (x + y)
+    if f'{x},{y},mod' not in cache:
+        cache[f'{x},{y},mod'] = cache[f'{x},{y},div'] % 982451653
+    return cache[f'{x},{y},mod']
 
 
 
